03b00188597556cceaa7b2183717eabcaabffef8
Gabriel Apostu <gabiapostu@yahoo.com>
2024-11-23 22:25:06 +0200

    build commit

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,26 +1,26 @@
-import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { auth } from "@clerk/nextjs";
+import { createUploadthing, type FileRouter } from 'uploadthing/next';
+// import { auth } from "@clerk/nextjs";
 
 const f = createUploadthing();
 
-const handleAuth = () => {
-  const { userId } = auth();
+// const handleAuth = () => {
+//   const { userId } = auth();
 
-  if (!userId) throw new Error("Unauthorized");
+//   if (!userId) throw new Error("Unauthorized");
 
-  return { userId };
-};
+//   return { userId };
+// };
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   image: f({ image: { maxFileCount: 6 }, video: { maxFileCount: 3 } })
     // Set permissions and file types for this FileRoute
-    .middleware(() => handleAuth())
+    // .middleware(() => handleAuth())
     .onUploadComplete(() => {}),
-  file: f(["image", "video", "audio", "pdf"])
+  file: f(['image', 'video', 'audio', 'pdf'])
     // Set permissions and file types for this FileRoute
-    .middleware(() => handleAuth())
+    // .middleware(() => handleAuth())
     .onUploadComplete(() => {}),
 } satisfies FileRouter;
 
